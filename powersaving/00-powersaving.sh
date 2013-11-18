4179dfab17a13de4b68b6700f03ca7c785fe583a
Robert Orzanna <orschiro@gmail.com>
2013-11-18 22:20:27 +0100

    Added lid action suspend/ignore feature

--- powersaving/00-powersaving.sh
+++ powersaving/00-powersaving.sh
@@ -6,7 +6,7 @@ case $1/$2 in
   post/*)
     echo "Waking up from $2..."
     echo "Calling powersaving after suspension / hibernation in 20 seconds"
-    sleep 20 
+    #sleep 20 
     /usr/bin/powersaving
     ;;
 esac
